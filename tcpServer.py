ea57a910ac0724897d94f590345c715aa40ac0bd
jonpol01 <johnpaul@soliva.me>
2016-06-14 15:53:00 +0900

    TCP server

---
+++ tcpServer.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+# substitute the TCP port you want to listen on for 10000 below
+server_address = ('', 50001)
+
+print >>sys.stderr, 'connecting to %s port %s' % server_address
+#sock.connect(server_address)
+sock.bind(('', 50000))
+# Listen for incoming connections
+sock.listen(1)
+
+while True:
+    # Wait for a connection
+    print >>sys.stderr, 'waiting for a connection'
+    connection, client_address = sock.accept()
+
+    try:
+        print >>sys.stderr, 'connection from', client_address
+
+        # Receive the data in small chunks and retransmit it
+        while True:
+            data = connection.recv(16)
+            print >>sys.stderr, 'received "%s"' % data
+            if data:
+                print >>sys.stderr, 'Data received - starting other Python script'
+		connection.send("$> " + data)
+            else:
+                print >>sys.stderr, 'no more data from', client_address
+                break
+
+    finally:
+        # Clean up the connection
+        connection.close()
