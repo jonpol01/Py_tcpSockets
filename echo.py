7a09d29892ee53e97fa422d1ffa31bfffe271451
jonpol01 <soliva.johnpaul@icloud.com>
2016-06-14 17:41:44 +0900

    Add files via upload

--- echo.py
+++ echo.py
@@ -1,7 +1,9 @@
-
+#!/usr/bin/python
 import asyncore
 import socket
 
+a = [0 for x in range(10)]
+i = 0
 class EchoHandler(asyncore.dispatcher_with_send):
 
     def handle_read(self):
@@ -23,7 +25,12 @@ class EchoServer(asyncore.dispatcher):
         if pair is not None:
             sock, addr = pair
             print 'Incoming connection from %s' % repr(addr)
-            handler = EchoHandler(sock)
+	    if i == 0:
+		a[0] = sock
+	    else:
+		i+1
+	    	a[i] = sock
+            handler = EchoHandler(a[i])
 
 server = EchoServer('', 8080)
 asyncore.loop()
